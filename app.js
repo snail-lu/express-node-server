43cd5f1b54aa6af5fb1a3034ab41c56ab28d4d7a
Snail-Lu <snaillu@yeah.net>
2022-07-31 15:37:52 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -89,4 +89,15 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/**
+ * 跨域问题
+ */
+app.all('*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+});
+
 module.exports = app;
