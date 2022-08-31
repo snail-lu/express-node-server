e937c0c4c4cc69c3bda94d15dbdfe776ec940ac0
Snail-Lu <snaillu@yeah.net>
2022-08-31 21:59:00 +0800

    字段值类型修改

--- models/admin.js
+++ models/admin.js
@@ -10,8 +10,8 @@ const adminSchema = mongoose.Schema({
     username: { type:String, required:true },       // 用户名
     password: { type:String, required:true },       // 密码
     email: { type:String, required:true },          // 邮箱
-    level: { type:String, required:true } ,         // 职位权限
-    status:{ type:String, required: true }          // 状态
+    level: { type:Number, required:true } ,         // 职位权限
+    status:{ type:Number, required: true }          // 状态
 })
 
 // 3 定义Model
